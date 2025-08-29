455f3d584a45a52c8eb4dc676dabdaae997d011d
Kasun_Udayanga <udayangakasun696@gmail.com>
2025-08-29 12:22:56 +0530

    Refactor code structure for improved readability and maintainability

---
+++ Pasindu/co.py
@@ -0,0 +1,17 @@
+input_file = "raw.conll"      # your original file
+output_file = "clean_datasetaa.conll"   # cleaned dataset
+
+with open(input_file, "r", encoding="utf-8") as infile, open(output_file, "w", encoding="utf-8") as outfile:
+    for line in infile:
+        line = line.strip()
+        if not line:
+            outfile.write("\n")  # keep sentence boundaries
+            continue
+        if line.startswith("-DOCSTART-"):  # skip DOCSTART
+            continue
+        parts = line.split()
+        if len(parts) >= 2:
+            word = parts[0]
+            label = parts[-1]  # last column is NER label
+            outfile.write(f"{word} {label}\n")
+#create as for using as dataset
